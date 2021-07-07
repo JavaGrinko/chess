0e1f4183d297a34c9aa9e10b7ff156e9efe3f5bf
Artem Grinko <asgrinko@alfabank.ru>
2021-07-07 16:33:54 +0300

    красивая менюшка

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let level;
+
 window.onload = () => {
     let table = document.getElementById('game-field');
     for (let i = 0; i < 8; i++) {
@@ -10,6 +12,34 @@ window.onload = () => {
         table.appendChild(tr);
     }
     renderFigure();
+    initMenu();
+}
+
+function initMenu() {
+    document.getElementById("button-monkey").onclick = () => {
+        level = 0;
+        startGame();
+    }
+    document.getElementById("button-beginner").onclick = () => {
+        level = 1;
+        startGame();
+    }
+    document.getElementById("button-intermediate").onclick = () => {
+        level = 2;
+        startGame();
+    }
+    document.getElementById("button-advanced").onclick = () => {
+        level = 3;
+        startGame();
+    }
+    document.getElementById("button-experienced").onclick = () => {
+        level = 4;
+        startGame();
+    }
+}
+
+function startGame() {
+    document.getElementById("menu").style["display"] = "none";
 }
 
 /*
@@ -44,13 +74,45 @@ function renderFigure() {
     for (let row = 0; row < 8; row++) {
         for (let column = 0; column < 8; column++) {
             switch (field[row][column]) {
+                case 0:
+                    putFigure(row, column, undefined);
+                    break;
                 case 1:
-                    putFigure(row, column, 'white-pawn');
+                    putFigure(row, column, "white-pawn")
                     break;
-                case 2: 
-                    putFigure(row, column, 'white-horse');
+                case 2:
+                    putFigure(row, column, "white-horse")
                     break;
-                //  дописать кейсы для 
+                case 3:
+                    putFigure(row, column, "white-elefan")
+                    break;
+                case 4:
+                    putFigure(row, column, "white-rook")
+                    break;
+                case 5:
+                    putFigure(row, column, "white_queen")
+                    break;
+                case 6:
+                    putFigure(row, column, "white-king")
+                    break;
+                case 11:
+                    putFigure(row, column, "black-pawn")
+                    break;
+                case 12:
+                    putFigure(row, column, "black-horse")
+                    break;
+                case 13:
+                    putFigure(row, column, "black-elefan")
+                    break;
+                case 14:
+                    putFigure(row, column, "black-rook")
+                    break;
+                case 15:
+                    putFigure(row, column, "black_queen")
+                    break;
+                case 16:
+                    putFigure(row, column, "black-king")
+                    break;
             }
         }
     }
@@ -75,7 +137,9 @@ function clickFigure(row, column) {
     } else {
         if (isEmpty(row, column)) {
             field[row][column] = field[lastFigure.row][lastFigure.column];
+            field[lastFigure.row][lastFigure.column] = 0;
             lastFigure = undefined;
+
         }
     }
     renderFigure();
